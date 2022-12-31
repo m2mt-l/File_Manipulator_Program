ff296f7daf228b37de12840e3c71d4401c1ac43d
m2mt-l <ta28ma2@gmail.com>
2022-12-31 03:11:46 +0000

    feat: add reverse action

---
+++ file_manipulator.py
@@ -0,0 +1,61 @@
+import sys
+from argparse import ArgumentParser, Action, Namespace
+from os.path import exists
+
+
+class ReverseAction(Action):
+    def __call__(
+        self,
+        parser: ArgumentParser,
+        namespace: Namespace,
+        values: list,
+        option_string: str,
+    ):
+        # print(f"parse: {parser} , type: {type(parser)}")
+        # print(f"parse: {namespace} , type: {type(namespace)}")
+        # print(f"parse: {values} , type: {type(values)}")
+        # print(option_string)
+        input_path = values[0]
+        output_path = values[1]
+        print(input_path)
+        if not exists(input_path):
+            print(f"Input file {input_path} does not exist")
+            return
+        contents = ""
+
+        with open(input_path) as f:
+            contents = f.read()
+
+        with open(output_path, "x") as f:
+            f.write(contents[::-1])
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--reverse",
+        type=str,
+        nargs=2,
+        action=ReverseAction,
+        metavar=("[input file]", "[output file]"),
+        help="reverse",
+    )
+
+    parser.add_argument(
+        "--copy", type=str, nargs=2, help="--copy [input file] [output file]"
+    )
+
+    parser.add_argument(
+        "--duplicate-contents",
+        type=str,
+        nargs=2,
+        help="--duplicate-contents [input file] [n]",
+    )
+
+    parser.add_argument(
+        "--replace-string",
+        type=str,
+        nargs=3,
+        help="--replace-string [input file] [needle] [newstring]",
+    )
+    args = parser.parse_args()
